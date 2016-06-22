57801e4f3a71beafc88ed0cb24b10bf5babe0c1b
tangxuyang <tangxuyang@guanaihui.com>
2016-06-22 18:16:55 +0800

    fix bug

--- jQuery.ajaxPM.js
+++ jQuery.ajaxPM.js
@@ -1,53 +1,52 @@
-/*
-	author:txy
-	email:tangxuyang.hi@163.com
-	date:2016-06-22
-*/
-
 +function($){
-	$.ajaxPM = function(options){
-		var iframe = $('iframe[src="'+options.middleUrl+'"]');
-		if(iframe.length === 0){
-			iframe = $('<iframe></iframe>');
-			iframe.src=options.middleUrl;	
-			iframe.css('display','none').appendTo($('body'));		
-		}
-
-		iframe = iframe[0];
+	function postMessage(iframe,options){
+		//iframe = iframe[0];
 
 		var win = iframe.contentWindow;
 		
-		var rdm = parseInt(Math.random()*100000000);//这个数字是我随便写的
+		var rdm = parseInt(Math.random()*100000000);
 		var successEventName ='success'+rdm,errorEventName="error"+rdm;
 		var successMethod = options.success, errorMethod = options.error;
 		options.success = successEventName;
 		options.error = errorEventName;
-		win.postMessage(options,"*");//发送请求给iframe
+		win.postMessage(options,"*");
 
-		//成功响应的函数
+
 		$('body').one(successEventName,function(){
 			if(successMethod){
 				successMethod.call(null,arguments[1]);
 			}
 		});
 
-		//失败响应函数
 		$('body').one(errorEventName,function(){
 			if(errorMethod){
 				errorMethod.call(null,arguments[1]);
 			}
 		});
+	}
+
+	$.ajaxPM = function(options){
+		var iframe = $('iframe[src="'+options.middleUrl+'"]');
+		if(iframe.length === 0){
+			iframe = $('<iframe></iframe>');
+			iframe.attr('src',options.middleUrl);	
+			iframe.css('display','none').appendTo($('body'));	
+			iframe.on('load',function(){
+				postMessage(iframe[0],options);
+			});	
+		}
+		else{
+			postMessage(iframe[0],options);
+		}
 	};
 
-	//初始化调用方
 	$.ajaxPM.initClient = function(){
 		$(window).on('message',function(e){
 			var data = e.originalEvent.data;
 			$('body').trigger(data.callback,data.result);		
 		});
-	}
+	};
 
-	//初始化中间人页面
 	$.ajaxPM.initMiddle = function(){
 		$(window).on('message',function(e){
 			var options = e.originalEvent.data;
@@ -71,5 +70,5 @@
 
 			$.ajax(options);
 		});
-	}
+	};
 }(jQuery);
